904ac3d80b9cf316e2a9d7e78f35751c4290ca4a
Canned-Seagull23 <101097344+Canned-Seagull23@users.noreply.github.com>
2022-03-15 18:35:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-
+//Variables
+const words = ["depth",
+               "boost",
+               "shark",
+               "ocean",
+               "charge",
+               "whale",
+               "squid",
+               ];
+//Utility Functions
+const random = {
+  integer: function(max, min) {
+    return Math.floor(Math.random() * max) + min;
+  },
+  object: function(array) {
+    return array[random.integer(array.length)];
+  }
+};
