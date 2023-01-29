c5f4ea6c8bc68c9f28b1150e4735a4167ea6aa83
Canned Tuna <101097344+Canned-Seagull23@users.noreply.github.com>
2023-01-29 21:05:31 +0800

    Fix some errors

--- js/script.js
+++ js/script.js
@@ -1,36 +1,30 @@
 //Variables
 const words = ["depth",
-               "ocean",
-               "water",
-               "coral",
-               "algae",
-               "boost",
-               "shark",
-               "whale",
-               "squid",
-               "hippo",
-               "giant",
-               "manta",
-               "polar",
-               "jelly",
-               "snake",
-               "bleed",
-               "coins",
-               "score",
-               "skins"
-               ];
-function setCookie(name, value) {
-  const d = new Date();
-  d.setTime(d.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
-  let expiry = d.toUTCString();
-  document.cookie = `${name}=${value}; expires=${expiry}; path=${path}`
-};
+  "ocean",
+  "water",
+  "coral",
+  "algae",
+  "boost",
+  "shark",
+  "whale",
+  "squid",
+  "hippo",
+  "giant",
+  "manta",
+  "polar",
+  "jelly",
+  "snake",
+  "bleed",
+  "coins",
+  "score",
+  "skins"
+];
 //Utility Functions
 const random = {
-  integer: function(max, min) {
+  integer: function (max, min) {
     return Math.floor(Math.random() * max) + min;
   },
-  object: function(array) {
+  object: function (array) {
     return array[random.integer(array.length, 0)];
   }
 };
@@ -41,31 +35,12 @@ let currentLine = 1;
 let won = false;
 //Regenerate Wordle
 const generateNewWord = () => {
-  try {
-    currentWord = (() => {
-      let name = "word=";
-      let decodedCookie = decodeURIComponent(document.cookie);
-      let ca = decodedCookie.split(';');
-      for (let i = 0; i < ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-              c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-              return c.substring(name.length, c.length);
-          }
-      }
-      throw '';
-    })();
-  } catch (e) {
-    currentWord = random.object(words);
-    currentWordArray = currentWord.split("");
-  };
-  document.cookie = `word=${currentWord}`;
+  currentWord = random.object(words);
+  currentWordArray = currentWord.split("");
   currentInput = [];
   currentLine = 1;
   won = false;
-  for(let line = 1; line < 7; line++) {
+  for (let line = 1; line < 7; line++) {
     let box1 = document.getElementById(line + "_1");
     let box2 = document.getElementById(line + "_2");
     let box3 = document.getElementById(line + "_3");
@@ -101,32 +76,32 @@ generateNewWord();
 let keypressed = {};
 const processInput = (keyPressed) => {
   let key = keyPressed.key.toLowerCase();
-  if(keyPressed.code == "Backspace") {
+  if (keyPressed.code == "Backspace") {
     currentInput.pop();
     addInput();
-  } else if(keyPressed.code == "Enter") {
+  } else if (keyPressed.code == "Enter") {
     enterInput();
     revealAnswers();
-  } else if(currentInput.length < 5) {
+  } else if (currentInput.length < 5) {
     checkValidChar(keyPressed);
     addInput();
   };
 };
 //Process Input
 function addInput() {
-  if(won == false) {
+  if (won == false) {
     updateUI(currentLine);
   };
 };
 function checkValidChar(keyPressed) {
   let key = keyPressed.code;
-  if(key == "KeyA" || key == "KeyB" || key == "KeyC" || key == "KeyD" || key == "KeyE" || key == "KeyF" || key == "KeyG" || key == "KeyH" || key == "KeyI" || key == "KeyJ" || key == "KeyK" || key == "KeyL" || key == "KeyM" || key == "KeyN" || key == "KeyO" || key == "KeyP" || key == "KeyQ" || key == "KeyR" || key == "KeyS" || key == "KeyT" || key == "KeyU" || key == "KeyV" || key == "KeyW" || key == "KeyX" || key == "KeyY" || key == "KeyZ") {
-     currentInput.push(keyPressed.key.toLowerCase());
+  if (key == "KeyA" || key == "KeyB" || key == "KeyC" || key == "KeyD" || key == "KeyE" || key == "KeyF" || key == "KeyG" || key == "KeyH" || key == "KeyI" || key == "KeyJ" || key == "KeyK" || key == "KeyL" || key == "KeyM" || key == "KeyN" || key == "KeyO" || key == "KeyP" || key == "KeyQ" || key == "KeyR" || key == "KeyS" || key == "KeyT" || key == "KeyU" || key == "KeyV" || key == "KeyW" || key == "KeyX" || key == "KeyY" || key == "KeyZ") {
+    currentInput.push(keyPressed.key.toLowerCase());
   };
 };
 function enterInput() {
-  if(won == true) {return;};
-  if(currentInput.length == 5 && currentLine != 7) {
+  if (won == true) { return; };
+  if (currentInput.length == 5 && currentLine != 7) {
     checkAnswers(currentLine);
     currentLine += 1;
     currentInput = [];
@@ -135,43 +110,43 @@ function enterInput() {
   };
 };
 function revealAnswers() {
-  if(currentLine == 7 && won === false) {
+  if (currentLine == 7 && won === false) {
     alert("The correct answer was \"" + currentWord + "\"");
   };
 };
 document.addEventListener("keydown", processInput, false);
 //Update UI
 function updateUI(currentLine) {
-  for(let clearBox = 1; clearBox < 6; clearBox++) {
+  for (let clearBox = 1; clearBox < 6; clearBox++) {
     let currentClearBox = document.getElementById(currentLine + "_" + clearBox);
     currentClearBox.innerHTML = "";
   };
-  for(let box = 1; box < currentInput.length + 1; box++) {
+  for (let box = 1; box < currentInput.length + 1; box++) {
     let currentBox = document.getElementById(currentLine + "_" + box);
     currentBox.innerHTML = currentInput[box - 1].toUpperCase();
   };
 };
 //Check Answers
 function checkAnswers(currentLine) {
-  for(let box = 1; box < 6; box++) {
+  for (let box = 1; box < 6; box++) {
     let currentBox = document.getElementById(currentLine + "_" + box);
     currentBox.setAttribute("class", "boxIncorrect");
-    if(currentInput[box - 1] == currentWordArray[box - 1]) {
+    if (currentInput[box - 1] == currentWordArray[box - 1]) {
       currentBox.setAttribute("class", "boxCorrect");
     } else {
-      for(let arrayIndex = 0; arrayIndex < 5; arrayIndex++) {
-        if(currentInput[box - 1] == currentWordArray[arrayIndex]) {
+      for (let arrayIndex = 0; arrayIndex < 5; arrayIndex++) {
+        if (currentInput[box - 1] == currentWordArray[arrayIndex]) {
           currentBox.setAttribute("class", "boxYellow");
         };
       };
     };
-    if(box == 5) {
+    if (box == 5) {
       let box1 = document.getElementById(currentLine + "_1").innerHTML.toLowerCase();
       let box2 = document.getElementById(currentLine + "_2").innerHTML.toLowerCase();
       let box3 = document.getElementById(currentLine + "_3").innerHTML.toLowerCase();
       let box4 = document.getElementById(currentLine + "_4").innerHTML.toLowerCase();
       let box5 = document.getElementById(currentLine + "_5").innerHTML.toLowerCase();
-      if(box1 === currentWordArray[0] && box2 === currentWordArray[1] && box3 === currentWordArray[2] && box4 === currentWordArray[3] && box5 === currentWordArray[4]) {
+      if (box1 === currentWordArray[0] && box2 === currentWordArray[1] && box3 === currentWordArray[2] && box4 === currentWordArray[3] && box5 === currentWordArray[4]) {
         setTimeout(() => alert("Correct!"), 100);
         won = true;
       };
